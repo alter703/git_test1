50a47584561091641a1498e622a9628ca9016580
Anatoliy <anatoliy.brazh130@gmail.com>
2024-02-28 22:58:25 +0200

    Adding the folder with text file, upgrading start.py and creating another_cats.py

---
+++ another_cats.py
@@ -0,0 +1,8 @@
+from start import Cat
+
+just_cat = Cat('Me', 3)
+print(just_cat.getmeow())
+
+second_meow = Cat('You', 4)
+print(second_meow.getmeow())
+print(second_meow.log_cat())
--- start.py
+++ start.py
@@ -6,6 +6,10 @@ class Cat:
     def getmeow(self):
         return f"The cat {self.name}, {self.age} years old says 'Meow!'."
 
+    def log_cat(self):
+        return {'name': self.name, 'age': self.age}
 
-cat_1 = Cat('Молі', 3)
-print(cat_1.getmeow())
+
+if __name__ == '__main__':
+    cat_1 = Cat('Молі', 3)
+    print(cat_1.getmeow())
